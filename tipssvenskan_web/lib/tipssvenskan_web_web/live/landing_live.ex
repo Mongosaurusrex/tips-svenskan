590bc97ee1d87aa5fdd9628af2e3a5c5678b9ff3
Nathan Dygant <nathan.dygant@gmail.com>
2025-08-10 22:27:08 +0200

    Adding the base for the landing site

--- tipssvenskan_web/lib/tipssvenskan_web_web/live/landing_live.ex
+++ tipssvenskan_web/lib/tipssvenskan_web_web/live/landing_live.ex
@@ -2,6 +2,17 @@ defmodule TipssvenskanWebWeb.LandingLive do
   use TipssvenskanWebWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :page_title, "Tipssvenskan")}
+    {:ok,
+     socket
+     |> assign(:page_title, "Tipssvenskan – Tippa slutställningen")
+     |> assign(:cta_loading, false)}
+  end
+
+  def handle_event("start", _params, socket) do
+    {:noreply, push_navigate(socket, to: ~p"/register")}
+  end
+
+  def handle_event("learn_more", _params, socket) do
+    {:noreply, push_navigate(socket, to: ~p"/about")}
   end
 end
